d5cf6f9b2931a8fe1fafdbceacccaf2db078bce0
hiroaki-com <comukichi@gmail.com>
2022-12-22 00:48:52 +0900

    PEP

--- duplicates.py
+++ duplicates.py
@@ -2,8 +2,16 @@ import pandas as pd
 import codecs
 
 
-data = pd.read_csv('file.csv', low_memory=False)
+data = pd.read_csv(
+    'file.csv', low_memory=False
+    )
 df = pd.DataFrame(data)
-duplicates = df.pivot_table(index =['first_name'], aggfunc = 'size')
+duplicates = df.pivot_table(
+    index =['name'], aggfunc = 'size'
+    )
 pd.set_option("display.max_rows",None)
-print(duplicates, file=codecs.open('New_file.csv', 'w', 'utf-8'))
+print(duplicates, file=codecs.open(
+    'New_file.csv', 'w', 'utf-8')
+    )
+
+
